b7ec9bcf6d7d398e2ffc7a5a0f80248aab184264
wdadadawd <3056786023@qq.com>
2022-10-09 16:50:04 +0800

    first commit

---
+++ src/main/java/com/lsu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.lsu.git;
+
+/**
+ * @author zt
+ * @create 2022-10-09 16:35
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,world");
+    }
+}
